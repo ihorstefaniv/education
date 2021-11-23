528df1ffe5729da5a28d4af322b9184162ff8cda
Stefaniv <dezir16@gmail.com>
2021-11-24 00:06:03 +0200

    Added tasks 1-4

---
+++ HOMEWORK1/task3.js
@@ -0,0 +1,9 @@
+let firstBox = "cola";
+let secondBox = "burger";
+
+console.log(firstBox);
+console.log(secondBox);
+
+firstBox = secondBox;
+
+console.log(firstBox);
---
+++ HOMEWORK1/task4.js
@@ -0,0 +1,9 @@
+let dog = {
+    name: "Jerry",
+    age: 4,
+    big: true,
+    birthday: "",
+    breed: null,
+};
+
+console.log(dog);
