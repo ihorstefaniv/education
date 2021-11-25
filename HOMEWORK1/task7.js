ae8409d84901ea4aed7b9a3cabfe40e4be724dcc
Stefaniv <dezir16@gmail.com>
2021-11-25 23:42:34 +0200

    added tasks 5-8(homework1)

---
+++ HOMEWORK1/task7.js
@@ -0,0 +1,4 @@
+let user = prompt("You'r login?", "John");
+let email = prompt("You'r email?", "email");
+let password = prompt("You'r password?", "password");
+alert("Dear " + user + ", your email is " + email + ", your password is " + password);
