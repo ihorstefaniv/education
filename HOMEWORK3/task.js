f1d47cb970785c013eda8e2756cc9201304e888b
Stefaniv <dezir16@gmail.com>
2021-12-06 17:53:59 +0200

    added homework 3

--- HOMEWORK3/task.js
+++ HOMEWORK3/task.js
@@ -5,4 +5,22 @@ for (let i = 1; i < arr.length; i++ ) {
     result *= arr[i];
 }
 
-console.log(result); 
+console.log(result);
+
+//Task 2
+for (i = 0; i <= 15; i++) {
+    if (i % 2 == 0){
+        console.log(i + " is even" );
+    } else {
+        console.log(i + " is odd" );
+    }
+}
+//Task 3
+let k = [];
+
+function randArray() {
+    for (var i = 1; i <= 500; i++) {
+        console.log(randomNumber(0, 10))
+      }
+} 
+
