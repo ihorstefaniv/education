fceef3138a716f322e28db8d6f3a704ae1c4ab7a
Stefaniv <dezir16@gmail.com>
2021-12-06 20:05:10 +0200

    change tasks

--- HOMEWORK3/task.js
+++ HOMEWORK3/task.js
@@ -9,18 +9,82 @@ console.log(result);
 
 //Task 2
 for (i = 0; i <= 15; i++) {
-    if (i % 2 == 0){
-        console.log(i + " is even" );
+    if (i % 2 == 0) {
+        console.log(i + " is even");
     } else {
-        console.log(i + " is odd" );
+        console.log(i + " is odd");
     }
 }
 //Task 3
-let k = [];
+function randArray(k) {
+    const m = [];
+    for (i = 0; i < k; i++) {
+        m[i] = Math.floor(Math.random() * 500) + 1;
+    }
+    console.log(m);
+}
+randArray(5);
 
-function randArray() {
-    for (var i = 1; i <= 500; i++) {
-        console.log(randomNumber(0, 10))
+// task4
+
+function raiseToDegree(a, b) {
+    let degree = a;
+     for (let i = 1; i < b; i++) {
+        degree *=a;
       }
-} 
+      return degree;
+}
 
+let a = +prompt("Введіть число a", "");
+let b = +prompt("Введіть число b", "");
+
+if (Number.isInteger(a) && Number.isInteger(b)) {
+    console.log(raiseToDegree(a, b));
+} else {
+    console.log("Некоректні дані!");
+}
+raiseToDegree(a, b);
+
+//task5
+
+function findMin() {
+    let min = arguments[0];
+    for (i = 1; i < arguments.length; i++) {
+if (arguments[i] < min) {
+   min = arguments[i] ;
+    }
+}
+return min;
+}
+console.log( findMin(13, 14, 4, 4, -0.2) );
+
+// task6
+function findUnique(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let k = (i + 1); k < array.length; k++) {
+            if (array[i] === array[k]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(findUnique([1, 2, 3, 5, 2]));
+console.log(findUnique([1, 2, 3, 5, 11]));
+
+//task 7
+
+
+//task8 
+function firstUpperCase(text) {
+    let newArray = text.split (" ");
+    let arr = [];
+
+    for (let i = 0; i < newArray.length; i++) {
+        arr.push(newArray[i].charAt(0).toUpperCase()+ newArray[i].slice(1));
+    }
+    return arr.join(" ");
+}
+
+console.log(firstUpperCase("i love javascript" ));
